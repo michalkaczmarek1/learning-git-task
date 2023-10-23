3c29f3d664759a9b8d9da8498675b6273f5dd55a
Michał Kaczmarek <michalkaczmarek788@gmail.com>
2023-10-23 20:12:20 +0200

    Init project

---
+++ lista_zakupow.py
@@ -0,0 +1,14 @@
+shopping_list = {
+    "piekarnia": ['chleb', 'bułki', 'pączek'],
+    "warzywniak": ['marchew', 'seler', 'rukola']
+}
+
+print("Lista zakupów")
+amount_products = 0
+for place, products in shopping_list.items():
+    products_capitalize = [product.capitalize() for product in products]
+    amount_products += len(products)
+    print(f"Ide do {place.capitalize()} i kupuję tam następujące rzeczy: {products_capitalize}")
+print(f"W sumie kupuję {str(amount_products)} produktów")
+
+    
