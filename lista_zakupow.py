6a73b8f046938a27ddb2396b9219558ebbfdd704
Michał Kaczmarek <michalkaczmarek788@gmail.com>
2023-10-23 20:27:30 +0200

    Add amount products and display it

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -4,7 +4,9 @@ shopping_list = {
 }
 
 print("Lista zakupów")
-
+amount_products = 0
 for place, products in shopping_list.items():
     products_capitalize = [product.capitalize() for product in products]
+    amount_products += len(products)
     print(f"Ide do {place.capitalize()} i kupuję tam następujące rzeczy: {products_capitalize}")
+print(f"W sumie kupuję {str(amount_products)} produktów")
