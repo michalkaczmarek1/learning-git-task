eed5125bea07d8705868e0855c6a17ae688a596e
Michał Kaczmarek <michalkaczmarek788@gmail.com>
2023-10-23 20:20:56 +0200

    Iterate dictionary and display specific text

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -2,3 +2,9 @@ shopping_list = {
     "piekarnia": ['chleb', 'bułki', 'pączek'],
     "warzywniak": ['marchew', 'seler', 'rukola']
 }
+
+print("Lista zakupów")
+
+for place, products in shopping_list.items():
+    products_capitalize = [product.capitalize() for product in products]
+    print(f"Ide do {place.capitalize()} i kupuję tam następujące rzeczy: {products_capitalize}")
